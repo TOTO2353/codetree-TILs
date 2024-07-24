c798d86a14d959c23e40ebe725cffaf1d2e53eaf
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-24 15:10:32 +0900

    Title: 변수 값 교체하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a=3; int temp =a;  int b=5;
+    
+    a=b;
+    b=temp;
+
+    cout << a << endl << b;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
