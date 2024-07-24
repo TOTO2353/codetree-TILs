6aad8cfa2e76a068fe2dc0da9378dbd275505890
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-24 18:07:57 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a;
+
+    if (a >=1) {
+        cout << "High";
+    }
+    else if (a >=0.5){
+        cout << "Middle";
+    }
+    else cout << "Low";
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
