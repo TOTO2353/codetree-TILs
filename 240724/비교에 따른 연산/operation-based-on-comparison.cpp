3b067d413cb3f3ce5c6ef4afcc418fa4d9f67fa8
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-24 17:34:54 +0900

    Title: 비교에 따른 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/비교에 따른 연산/operation-based-on-comparison.cpp
+++ 240724/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -5,12 +5,12 @@ int main() {
     int a,b;
     cin >>a>>b;
 
-    if (a>=b){
+    if (a>b){
         cout << a*b;
     }
 
     else {
-        cout << b%a;
+        cout << b/a;
     }
     // 여기에 코드를 작성해주세요.
     return 0;
