9f0fe7e1cf57fe68f66982a9678616721bd1107d
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-26 21:37:53 +0900

    Title: 좀 더 어려운 수학 점수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240726/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 240726/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -7,23 +7,11 @@ int main() {
 
     cin >> A_math >> A_English;
     cin >> B_math >> B_English;
-
-    if ((A_math == B_math) && (A_English !=B_English)){
-        if (A_English > B_English) {
-            cout << "A";
-        }
-
-        else cout << "B";
-    }
-
-    if (A_math != B_math) {
-        if ( A_math > B_math ){
-            cout << "A";
-        }
-
-        else cout << "B";
-    }
-    
+ 
+    if(A_math > B_math || (a_math == B_math && A_English > B_English))
+        cout <<"A";
+    else 
+        cout <<"B";
     // 여기에 코드를 작성해주세요.
     return 0;
 }
