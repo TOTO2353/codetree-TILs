c217ff2857e2d8736e5007e04e88fc13f54f65b0
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-26 22:40:36 +0900

    Title: 증가시키며 출력하기 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a=10;
+
+    while (a<27) {
+        cout << a << ' ';
+        a ++;
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
