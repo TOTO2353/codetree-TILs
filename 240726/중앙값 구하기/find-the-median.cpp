f4127dd57a067a601e1fef0dd207897c603722cb
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-26 22:20:42 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240726/중앙값 구하기/find-the-median.cpp
+++ 240726/중앙값 구하기/find-the-median.cpp
@@ -5,12 +5,12 @@ int main() {
     int a,b,c;
     cin >> a >>b >>c;
 
-    if ( ( a>=b && a<=c ) || ( a>=c && a<=b )){
+    if ( ( a>b && a<c ) || ( a>c && a<b )){
         cout<<a;
     }
     
 
-    else if ( b>=a && b>=c || b>=c && b>=a ){
+    else if ( b>a && b>c || b>c && b>a ){
         cout << b;
     }
 
