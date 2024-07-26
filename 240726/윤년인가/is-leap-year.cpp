a04f944e34df57eabbbec7e14621cbfe370a7fe7
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-26 22:31:52 +0900

    Title: 윤년인가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/윤년인가/is-leap-year.cpp
+++ 240726/윤년인가/is-leap-year.cpp
@@ -5,12 +5,10 @@ int main() {
     int y;
     cin >> y;
 
-    if (y%4 !=0){
-        if ( y%100 ==0 && y%400!=0) {
+    if (y%4 !=0 || ( y%100 ==0 && y%400!=0) ){
             cout << "false";
         }
 
-    }
     else cout << "true";
     
     return 0;
