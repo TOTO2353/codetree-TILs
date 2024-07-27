ff7d3b3485783872006b2c8557eb7200b51840c6
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-27 15:33:00 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240727/배수의 개수 2/number-of-multipliers-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int cnt =0;
+
+    for (int i=1; i<=10; i++){
+        cin >> n;
+        if (n%2!=0){
+            cnt ++;
+        }
+    }
+    cout << cnt ;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
