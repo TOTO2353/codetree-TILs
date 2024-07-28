021bab5d4eb90b2e22fd7a6041790ae4fdc41493
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-07-28 14:50:06 +0900

    Title: 정사각형 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240728/정사각형 출력/print-square.cpp
+++ 240728/정사각형 출력/print-square.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int cnt =0;
+    int cnt =1;
 
-    for ( int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
+    for ( int i=1; i<=n; i++){
+        for (int j=1; j<=n; j++){
             cout << cnt <<' ';
             cnt++;
         }
