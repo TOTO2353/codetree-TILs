99bd19d460d5bf33ac0366b17f9acc5c5b379092
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2024-08-05 19:19:42 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240805/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    int len1 = str1.length();
+    int len2 = str2.length();
+
+    cout << len1 + len2;
+
+    
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
