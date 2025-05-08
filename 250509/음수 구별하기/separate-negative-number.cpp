965d4c6b9fbcb841e6f942dce31009b04ed07156
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2025-05-09 02:01:27 +0900

    Title: 음수 구별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250509/음수 구별하기/separate-negative-number.cpp
+++ 250509/음수 구별하기/separate-negative-number.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 
 int main() {
-    int N
-    cin>>N
-    
-    if (N<0) {
-       cout << N << endl << 'minus'
-    }
+
+    int N;  
+    cin>>N;
+
+     if (N<0) {
+       cout << N << endl << "minus";
+     }
+
+     cout<<N;
  
   
      // Please write your code here.
