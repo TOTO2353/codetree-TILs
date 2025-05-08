9a64085e0dcf122ffa53caa538ea8b4959b75d0a
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2025-05-09 02:28:37 +0900

    Title: 등급 매기기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250509/등급 매기기/ranking.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N>=90){
+        cout << "A";
+    }
+
+    else if (80<=N<90){
+        cout << "B";
+    }
+
+    else if (70<=N<80){
+        cout <<"C";
+    }
+
+    else if (60<D<=70){cout <<"D"}
+
+    else cout << "F";
+    // Please write your code here.
+    return 0;
+}
