ee3f9084c0794482d45e167541bc8d9bc68725b1
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2025-05-09 02:11:33 +0900

    Title: 비교 연산, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250509/비교 연산/comparison-operator.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int A; int B;
+    cin>>A>>B;
+
+    if(A>=B){
+        cout << 1<< endl;
+    }
+    else { cout << 0 <<endl;}
+
+    if(A>B){
+        cout << 1 << endl;
+    }
+    else { cout << 0 <<endl;}
+
+    if(B>=A){
+        cout << 1 << endl;
+    }
+    else { cout << 0 <<endl;}
+
+    if(B>A){
+        cout << 1 << endl;
+    }
+    else { cout << 0 <<endl;}
+
+    if(A==B){
+        cout << 1 << endl;
+    }
+    else { cout << 0 <<endl;}
+
+    if(A!=B){
+        cout << 1 << endl;
+    }
+    else { cout << 0 <<endl;}
+
+    // Please write your code here.
+    return 0;
+}
