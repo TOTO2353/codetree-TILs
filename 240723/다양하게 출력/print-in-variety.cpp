5cb8a1b63374afdd0e170cbe3d8184492307d596
KIM CHAE YOON <163922217+TOTO2353@users.noreply.github.com>
2025-05-30 21:48:43 +0900

    Delete 240723 directory

--- 240723/다양하게 출력/print-in-variety.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Total days in Year" << endl << 365;
-    cout << endl;
-    cout << "Circumference rate" << endl << "3.1415926535";
-    return 0;
-}
--- 240723/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a =7; int b=23; int c=30;
-    cout << a << " + "<< b <<" = "<< c;
-    return 0;
-}
